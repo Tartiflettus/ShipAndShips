37faab2cb7785d82eaa77eac9a3f72ea06b8a035
Maureen <mo.mo88@hotmail.fr>
2018-03-28 18:02:52 +0200

    placementShip

--- src/model/strategy/PlacementStrategy.java
+++ src/model/strategy/PlacementStrategy.java
@@ -1,11 +1,13 @@
 package model.strategy;
 
+import java.util.List;
+
 import exception.NotPlaceableException;
 import model.BattleField;
 import model.ship.Ship;
 
 public interface PlacementStrategy {
 
-	public void placeShips(BattleField bf, Ship... ships) throws NotPlaceableException;
+	public void placeShips(BattleField bf, List<Ship> ships) throws NotPlaceableException;
 
 }
