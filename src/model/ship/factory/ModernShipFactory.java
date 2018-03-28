05331968142dbdcac1fc80eb8e79dd1af2a38308
Maureen <mo.mo88@hotmail.fr>
2018-03-28 16:50:40 +0200

    ModernShipFactory

--- src/model/ship/factory/ModernShipFactory.java
+++ src/model/ship/factory/ModernShipFactory.java
@@ -1,18 +1,39 @@
 package model.ship.factory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.ship.Ship;
+import model.ship.modern.Cruiser;
+import model.ship.modern.Gondola;
 
 public class ModernShipFactory implements ShipFactory {
 	public static final ModernShipFactory instance = new ModernShipFactory();
 	
-	public ModernShipFactory() {
-		// TODO Auto-generated constructor stub
+	private ArrayList<Ship> tabShip;
+	
+	private ModernShipFactory() {
+		
+		Ship ship1 = new Cruiser();
+		Ship ship2 = new Gondola();
+		
+		tabShip.add(ship1);
+		tabShip.add(ship2);
+		
+	}
+	
+/**
+ * return the list of Modern Ships
+ */
+	public List<Ship> getShips() {
+		return tabShip;
 	}
 
-	@Override
-	public Ship[] getShips() {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * 
+	 * @return the instance of ModernShipFactory
+	 */
+	public ModernShipFactory getInstance() {
+		return instance;
 	}
-
 }
